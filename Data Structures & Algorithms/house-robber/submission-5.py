124fa2900b95632ccf28d1f6957789fbced731c1
Jesse Li <53070256+Jess2772@users.noreply.github.com>
2026-05-26 18:35:18 -0400

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        #dp[n] represents how much money you can make from houses 0 ... n
+        N = len(nums)
+        if N == 1:
+            return nums[0]
+        dp = [0] * N
+
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+
+        for i in range (2, N):
+            dp[i] = max(dp[i- 1], nums[i] + dp[i - 2])
+
+        return dp[-1]
+
